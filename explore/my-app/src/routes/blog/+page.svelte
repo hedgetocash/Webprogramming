818467dd4b3cf5b7094e6b0d1749d64920f9d3e1
hedgetocash <n.schuepbach@icloud.com>
2026-01-13 14:57:13 +0100

    verschiedenste änderungen

---
+++ explore/my-app/src/routes/blog/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let { data } = $props()
+</script>
+
+<h1>blog post</h1>
+
+<ul>
+	<li><a href="/blog/one">one</a></li>
+	<li><a href="/blog/two">two</a></li>
+	<li><a href="/blog/three">three</a></li>
+	{#each data.summaries as { slug, title }}
+		<li><a href="/blog/{slug}">{title}</a></li>
+	{/each}
+</ul>
